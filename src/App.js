5f1620b163fa9d8d41e14210fcae5b305d337d45
Architgarg603 <architgarg603@gmail.com>
2022-02-06 21:25:29 +0530

    ML

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './Components/Homepage/Homepage';
+import Navbar from './Components/Navbar/Navbar';
 import Result from './Components/Results/Result';
 import Test from './Components/Test/Test';
 
 function App() {
   return (
       <BrowserRouter>
+      <Navbar />
       <Routes>
           <Route path='/test' element={<Test />} />
           <Route path='/result' element={<Result />} />
