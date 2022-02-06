00de93b6c12a7a19c6e97f50c64e511341196765
Architgarg603 <architgarg603@gmail.com>
2022-02-06 21:13:46 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './Components/Homepage/Homepage';
+import Result from './Components/Results/Result';
+import Test from './Components/Test/Test';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+      <Routes>
+          <Route path='/test' element={<Test />} />
+          <Route path='/result' element={<Result />} />
+          <Route path='/' element={<Homepage />} />
+      </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Homepage.module.scss'
+function Homepage() {
+  return <div className={style.wrapper}>
+      <div className={style.h1}>
+          <div></div>
+      </div>
+      <div className={style.h2}></div>
+      <div className={style.h3}></div>
+      <div className={style.h4}></div>
+  </div>;
+}
+
+export default Homepage;
